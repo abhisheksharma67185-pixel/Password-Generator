8084a4cb3545ee0ebb1cd93ee643bb8edafa9997
Abhishek Sharma <abhisheksharma67185@gmail.com>
2026-02-18 00:54:04 +0530

    Add files via upload

---
+++ password generator.py
@@ -0,0 +1,34 @@
+import random 
+import string
+string.ascii_letters = ("qwertyuiopasdfghjklzxcvbnm")
+print(string.ascii_letters[1:26])
+string.digits = "0123456789"
+print(string.digits[0:9])
+string.punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_{|}~'
+print(string.punctuation[1:9])
+pool = ("string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation")
+
+while True:
+    print('1: generate password')
+    print('2: custom length')
+    print('3: quit')
+    choice = int(input('enter choice:'))
+    if choice == 1:
+        password_chars = []
+        for i in range (12):
+            char = random.choice(pool)
+            password_chars.append(char)
+            password = ''.join(password_chars) 
+            print(f'generated: {password}')
+            input('press enter')
+    elif choice == 2:
+        password_chars = []
+        for i in range (length):
+            char = random.choice(pool)
+            password_chars.append(char)
+            password = ''.join(password_chars) 
+            print(f'generated: {password}')
+            input('press enter')
+        input('press enter')
+    elif choice == 3:
+        break
